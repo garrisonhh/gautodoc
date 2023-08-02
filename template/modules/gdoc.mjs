33e7e7350e5537648e3b004de4f60fb04597eaca
garrisonhh <garrisonhh@pm.me>
2023-08-02 14:05:36 -0400

    get sidebar working!

--- template/modules/gdoc.mjs
+++ template/modules/gdoc.mjs
@@ -39,7 +39,20 @@ function renderDoc(text) {
     return el;
 }
 
-function renderSig(declwith, name, sig) {
+function renderModuleSig(name, uname) {
+    const div = makeDiv('decl', [
+        makeSpan('none', name),
+    ]);
+
+    if (typeof uname !== 'undefined') {
+        div.id = uname;
+    }
+
+    return div;
+}
+
+/** renders sig. set uname to make this div jumpable */
+function renderSig(declwith, name, sig, uname) {
     const kids = [
         makeSpan('none', declwith + ' '),
         makeSpan('var', name),
@@ -79,22 +92,27 @@ function renderSig(declwith, name, sig) {
         kids.push(makeSpan('type', sig.returns));
     }
 
-    return makeDiv('decl', kids);
+    const div = makeDiv('decl', kids);
+    if (typeof uname !== 'undefined') {
+        div.id = uname;
+    }
+
+    return div;
 }
 
-function renderFunction({ name, sig, doc }) {
+function renderFunction({ name, uname, sig, doc }) {
     const kids = [];
 
-    kids.push(renderSig('def', name, sig));
+    kids.push(renderSig('def', name, sig, uname));
     if (doc) kids.push(renderDoc(doc));
 
     return makeDiv('function', kids);
 }
 
-function renderClass({ name, sig, doc, classes, functions }) {
+function renderClass({ name, uname, sig, doc, classes, functions }) {
     const kids = [];
 
-    kids.push(renderSig('class', name, sig));
+    kids.push(renderSig('class', name, sig, uname));
     if (doc) kids.push(renderDoc(doc));
 
     classes.forEach((meta) => {
@@ -108,13 +126,10 @@ function renderClass({ name, sig, doc, classes, functions }) {
     return makeDiv('class', kids);
 }
 
-function renderModule({ name, doc, classes, functions }) {
+function renderModule({ name, uname, doc, classes, functions }) {
     const kids = [];
 
-    kids.push(makeDiv('decl', [
-        makeSpan('none', name),
-    ]));
-
+    kids.push(renderModuleSig(name, uname));
     if (doc) kids.push(renderDoc(doc));
 
     classes.forEach((meta) => {
@@ -128,7 +143,54 @@ function renderModule({ name, doc, classes, functions }) {
     return makeDiv('module', kids);
 }
 
+// sidebar index ===============================================================
+
+function makeIndexNode(decl, kids) {
+    return makeDiv('index-node', [decl, ...kids]);
+}
+
+function renderIndexedDecl(declwith, name, sig, uname, kids) {
+    const decl = renderSig(declwith, name, sig);
+    decl.addEventListener('click', () => {
+        window.location.hash = uname;
+    });
+
+    return makeIndexNode(decl, kids)
+}
+
+function renderIndexedFunction({ name, sig, uname}) {
+    return renderIndexedDecl('def', name, sig, uname, []);
+}
+
+function renderIndexedClass({ name, sig, uname, functions, classes }) {
+    const iFunctions = functions.map(renderIndexedFunction);
+    const iClasses = classes.map(renderIndexedClass);
+    const kids = iFunctions.concat(iClasses);
+
+    return renderIndexedDecl('class', name, sig, uname, kids);
+}
+
+function renderIndexedModule({ name, uname, functions, classes }) {
+    const decl = renderModuleSig(name);
+    decl.addEventListener('click', () => {
+        window.location.hash = uname;
+    })
+
+    const iFunctions = functions.map(renderIndexedFunction);
+    const iClasses = classes.map(renderIndexedClass);
+    const kids = iFunctions.concat(iClasses);
+
+    return makeIndexNode(decl, kids);
+}
+
+function renderIndex(registry) {
+    return makeDiv('index', registry.modules.map(renderIndexedModule));
+}
+
 // =============================================================================
+
+/** used to create unique registry ids */
+const UNAMES = new Set();
 
 /** returns loaded json, or null if not found */
 async function fetchRegistry() {
@@ -136,6 +198,34 @@ async function fetchRegistry() {
     if (!res.ok) return null;
 
     return await res.json();
+}
+
+/** adds a 'uname' field to each object child that contains a 'name' field */
+function addUniqueNames(obj) {
+    if (Object.hasOwn(obj, "name")) {
+        const name = obj["name"];
+        let uname = name;
+        let idx = 0;
+
+        while (UNAMES.has(uname)) {
+            uname = `${name}${++idx}`;
+        }
+
+        UNAMES.add(uname);
+        obj["uname"] = uname;
+    }
+
+    for (const value of Object.values(obj)) {
+        if (typeof value === 'object' && value) {
+            addUniqueNames(value)
+        } else if (typeof value === 'array') {
+            value.forEach((el) => {
+                if (typeof el === 'object') {
+                    addUniqueNames(el)
+                }
+            });
+        }
+    }
 }
 
 /**
@@ -146,6 +236,10 @@ export default async function gdoc() {
     const reg = await fetchRegistry();
     if (!reg) return errorDiv(`couldn't fetch registry at ${REGISTRY_URL}`);
 
-    const modules = reg.modules.map((m) => renderModule(m));
-    return makeDiv('gdoc', modules);
+    addUniqueNames(reg);
+
+    return makeDiv('gdoc', [
+        renderIndex(reg),
+        makeDiv('modules', reg.modules.map(renderModule)),
+    ]);
 }
